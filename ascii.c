515fc258e9d2a8ba7e770d807b728b9b70215abb
igor <igorandrade@dcc.ufba.br>
2014-03-26 19:16:14 -0300

    atividade da lista
    teste

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int numASCII;
+	scanf("%d", &numASCII);
+
+	printf("Decimal: %i   Octal: %o   Hexadecimal: %x   Caractere: %c \n",numASCII,numASCII,numASCII,numASCII);
+	return 0;
+}
